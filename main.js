2da7818520eb188c92678b39c164a38b08afc823
kimkyeongnam <kkyy0126@naver.com>
2020-01-30 16:48:26 +0900

    Modify css, js, and korean_food.html

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-let value = 0;
+var result = 0;
 
-function getRandomInt(min, max){
-    return Math.floor(Math.random() * (max-min+1))+min;
+function getRandomInt(){
+    console.log(Math.floor(Math.random()*4) + 1);
+    return Math.floor(Math.random()*4) + 1;
+}
+
+function recommendFood(){
+    cleaning();
+    result = getRandomInt();
+    document.getElementById("food_"+result).style.backgroundColor = "lightyellow";
+}
+
+function cleaning(){
+    document.getElementById("food_1").style.backgroundColor = "FFFFFF";
+    document.getElementById("food_2").style.backgroundColor = "FFFFFF";
+    document.getElementById("food_3").style.backgroundColor = "FFFFFF";
+    document.getElementById("food_4").style.backgroundColor = "FFFFFF";
 }
